07771d40d43b441f943d4cdd03749eb3ef17437e
saintbassanaga <saintbassanaga01@gmail.com>
2023-05-10 02:37:07 +0100

    Rotate 2D matrix into 90*

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,28 +1,29 @@
-#!/usr/bin/env python3
-
-"""
-Test 0x07 - Rotate 2D Matrix
+#!/usr/bin/python3
+"""2D matrix rotation module.
 """
 
-C, R=3,3
-def reverseColumns(arr):
-    for i in range(C):
-        j = 0
-        k = C-1
-        while j < k:
-            t = arr[j][i]
-            arr[j][i] = arr[k][i]
-            arr[k][i] = t
-            j += 1
-            k -= 1
 
-def transpose(arr):
-    for i in range(R):
-        for j in range(i, C):
-            t = arr[i][j]
-            arr[i][j] = arr[j][i]
-            arr[j][i] = t
-
-def rotate_2d_matrix(mat):
-    reverseColumns(mat)
-    transpose(mat)
+def rotate_2d_matrix(matrix):
+    """Rotates an m by n 2D matrix in place.
+    """
+    if type(matrix) != list:
+        return
+    if len(matrix) <= 0:
+        return
+    if not all(map(lambda x: type(x) == list, matrix)):
+        return
+    rows = len(matrix)
+    cols = len(matrix[0])
+    if not all(map(lambda x: len(x) == cols, matrix)):
+        return
+    c, r = 0, rows - 1
+    for i in range(cols * rows):
+        if i % rows == 0:
+            matrix.append([])
+        if r == -1:
+            r = rows - 1
+            c += 1
+        matrix[-1].append(matrix[r][c])
+        if c == cols - 1 and r >= -1:
+            matrix.pop(r)
+        r -= 1
