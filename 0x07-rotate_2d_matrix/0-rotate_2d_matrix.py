6e4966c2e4eed26feaa6f13b6ca5a67ca7b89c91
saintbassanaga <saintbassanaga01@gmail.com>
2023-05-10 02:31:59 +0100

    Rotate 2D matrix into 90*

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,1 +1,26 @@
+#!/usr/bin/env python3
 
+C, R=3,3
+def reverseColumns(arr):
+    for i in range(C):
+        j = 0
+        k = C-1
+        while j < k:
+            t = arr[j][i]
+            arr[j][i] = arr[k][i]
+            arr[k][i] = t
+            j += 1
+            k -= 1
+
+def transpose(arr):
+    for i in range(R):
+        for j in range(i, C):
+            t = arr[i][j]
+            arr[i][j] = arr[j][i]
+            arr[j][i] = t
+
+def rotate_2d_matrix(mat):
+    reverseColumns(mat)
+    transpose(mat)
+
+    return mat
